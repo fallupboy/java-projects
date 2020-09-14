b3497c43cfff20c36ee193679a4144f1d30508e1
fallupboy <svity21@gmail.com>
2020-09-14 17:15:43 +0300

    upd
    -snake's model was changed (snake's tail movement has to be fixed)
    -borders collision was removed;
    -restart button was added (has to be fixed due to not restarting of active frame);
    -snake's speed was slightly reduced

--- SnakeGame/src/GameField.java
+++ SnakeGame/src/GameField.java
@@ -7,22 +7,43 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
-    private final int SIZE = 512;
+    private final int SIZE = 528;
     private final int DOT_SIZE = 16;
-    private final int ALL_DOTS = 1024;
-    private Image dot;
+    private final int ALL_DOTS = 1089;
     private Image apple;
+    private Image body;
+    private Image head;
+    private Image tail;
     private int appleX;
     private int appleY;
     private int[] x = new int[ALL_DOTS];
     private int[] y = new int[ALL_DOTS];
     private int dots;
+    private int tempXr = x[0];
+    private int tempYr = y[0];
+    private int tempXd = x[0];
+    private int tempYd = y[0];
+    private int tempXl = x[0];
+    private int tempYl = y[0];
+    private int tempXu = x[0];
+    private int tempYu = y[0];
     private Timer timer;
     private boolean left = false;
     private boolean right = true;
     private boolean up = false;
     private boolean down = false;
     private boolean inGame = true;
+
+    ImageIcon iia = new ImageIcon("apple.png");
+    ImageIcon iib = new ImageIcon("body.png");
+    ImageIcon iih_r = new ImageIcon("head_r.png");
+    ImageIcon iih_d = new ImageIcon("head_d.png");
+    ImageIcon iih_l = new ImageIcon("head_l.png");
+    ImageIcon iih_u = new ImageIcon("head_u.png");
+    ImageIcon iit_r = new ImageIcon("tail_r.png");
+    ImageIcon iit_d = new ImageIcon("tail_d.png");
+    ImageIcon iit_l = new ImageIcon("tail_l.png");
+    ImageIcon iit_u = new ImageIcon("tail_u.png");
 
     public GameField() {
         setBackground(Color.pink);
@@ -38,7 +59,7 @@ public class GameField extends JPanel implements ActionListener {
             x[i] = 16;
             y[i] = 16;
         }
-        timer = new Timer(50, this);
+        timer = new Timer(70, this);
         timer.start();
         createApple();
     }
@@ -49,10 +70,9 @@ public class GameField extends JPanel implements ActionListener {
     }
 
     public void loadImages() {
-        ImageIcon iia = new ImageIcon("apple.png");
         apple = iia.getImage();
-        ImageIcon iid = new ImageIcon("dot.png");
-        dot = iid.getImage();
+        body = iib.getImage();
+        tail = iit_r.getImage();
     }
 
     @Override
@@ -60,33 +80,55 @@ public class GameField extends JPanel implements ActionListener {
         super.paintComponent(g);
         if (inGame) {
             g.drawImage(apple, appleX, appleY, this);
-            for (int i = 0; i < dots; i++) {
-                g.drawImage(dot, x[i], y[i], this);
+            for (int i = 1; i < dots; i++) {
+                g.drawImage(body, x[i], y[i], this);
             }
+            g.drawImage(head, x[0], y[0], this);
+            g.drawImage(tail, x[dots], y[dots], this);
         } else {
             String str = "Game Over";
-//            Font f = new Font("Arial", 14, Font.BOLD);
+            String str2 = "Press 'Space' to restart";
+            Font f = new Font("Arial", Font.BOLD, 36);
             g.setColor(Color.white);
-//            g.setFont(f);
-            g.drawString(str, SIZE/2, SIZE/2);
+            g.setFont(f);
+            g.drawString(str, SIZE / 3, SIZE / 2);
+            g.drawString(str2, SIZE / 8, SIZE - 200);
         }
     }
 
     public void move() {
         for (int i = dots; i > 0; i--) {
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+
+            if (x[dots] == tempXl && y[dots] == tempYl) {
+                tail = iit_l.getImage();
+            }
+            if (x[dots] == tempXu && y[dots] == tempYu) {
+                tail = iit_u.getImage();
+            }
+            if (x[dots] == tempXr && y[dots] == tempYr) {
+                tail = iit_r.getImage();
+            }
+            if (x[dots] == tempXd && y[dots] == tempYd) {
+                tail = iit_d.getImage();
+            }
+
         }
-        if(left) {
+        if (left) {
+            head = iih_l.getImage();
             x[0] -= DOT_SIZE;
         }
-        if(right) {
+        if (right) {
+            head = iih_r.getImage();
             x[0] += DOT_SIZE;
         }
-        if(up) {
+        if (up) {
+            head = iih_u.getImage();
             y[0] -= DOT_SIZE;
         }
-        if(down) {
+        if (down) {
+            head = iih_d.getImage();
             y[0] += DOT_SIZE;
         }
     }
@@ -106,19 +148,19 @@ public class GameField extends JPanel implements ActionListener {
         }
 
         if (x[0] > SIZE) {
-            inGame = false;
+            x[0] = 0;
         }
+
         if (x[0] < 0) {
-            inGame = false;
+            x[0] = SIZE;
         }
         if (y[0] > SIZE) {
-            inGame = false;
+            y[0] = 0;
         }
         if (y[0] < 0) {
-            inGame = false;
+            y[0] = SIZE;
         }
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -135,28 +177,41 @@ public class GameField extends JPanel implements ActionListener {
         public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
             int key = e.getKeyCode();
-            if (key == KeyEvent.VK_LEFT && !right) {
+            if (key == KeyEvent.VK_LEFT && !right || key == KeyEvent.VK_A && !right) {
+                tempXl = x[0];
+                tempYl = y[0];
                 left = true;
                 up = false;
                 down = false;
             }
 
-            if (key == KeyEvent.VK_RIGHT && !left) {
+            if (key == KeyEvent.VK_RIGHT && !left || key == KeyEvent.VK_D && !left) {
+                tempXr = x[0];
+                tempYr = y[0];
                 right = true;
                 up = false;
                 down = false;
             }
 
-            if (key == KeyEvent.VK_UP && !down) {
+            if (key == KeyEvent.VK_UP && !down || key == KeyEvent.VK_W && !down) {
+                tempXu = x[0];
+                tempYu = y[0];
                 up = true;
                 right = false;
                 left = false;
             }
 
-            if (key == KeyEvent.VK_DOWN && !up) {
+            if (key == KeyEvent.VK_DOWN && !up || key == KeyEvent.VK_S && !up) {
+                tempXd = x[0];
+                tempYd = y[0];
                 down = true;
                 right = false;
                 left = false;
+            }
+
+            if (key == KeyEvent.VK_SPACE) {
+                MainWindow mw = new MainWindow();
+                mw.createUI();
             }
         }
     }
--- SnakeGame/src/MainWindow.java
+++ SnakeGame/src/MainWindow.java
@@ -1,17 +1,29 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
-    public MainWindow() {
-        setTitle("Snake");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(545, 565);
-        setLocationRelativeTo(null);
-        add(new GameField());
-        setVisible(true);
-        setResizable(false);
+    JFrame frame;
+
+    public void createUI() {
+        frame = new JFrame("Snake");
+        frame.setSize(556, 580);
+
+        Container pane = frame.getContentPane();
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        int x = (screenSize.width - frame.getWidth()) / 2;
+        int y = (screenSize.height - frame.getHeight()) / 2;
+        frame.setLocation(x, y);
+
+        pane.add(new GameField());
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
         MainWindow mw = new MainWindow();
+        mw.createUI();
     }
 }
